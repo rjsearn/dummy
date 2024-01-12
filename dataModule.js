c5ea9ed4685975d678fd25f5c7748510ab42d879
rjsearn <145315956+rjsearn@users.noreply.github.com>
2024-01-12 12:53:40 +0530

    Add files via upload

---
+++ dataModule.js
@@ -0,0 +1,31 @@
+// dataModule.js
+
+const headings = [
+    ["Here we are step1", "Hae you dne it", "Here we are ending about", "KRSNA"],
+    ["This is set2 starting", "Hae you dne it", "KRSNA", "Can nnot do it"],
+    ["set3 Can nnot do it", "MAAm Ekam", "Here we are starting"]
+];
+
+const images = [
+    ["https://i.imgur.com/VgvPhWq.png", "https://i.imgur.com/6LPnBYO.png",
+        "https://i.imgur.com/M4tjdab.png", "https://i.imgur.com/wuuoabf.png"
+    ],
+    ["https://i.imgur.com/dfNjudB.png", "https://i.imgur.com/6LPnBYO.png",
+        "https://i.imgur.com/M4tjdab.png", "https://i.imgur.com/lbengMy.png"
+    ],
+    ["https://i.imgur.com/dfNjudB.png", "https://i.imgur.com/LbnpjwK.png",
+        "https://i.imgur.com/M4tjdab.png"
+    ]
+];
+
+const links = [
+    ["https://example.com/link1", "https://example.com/link2", "https://example.com/link3",
+        "https://example.com/link4"
+    ],
+    ["https://example.com/link6", "https://example.com/link7", "https://example.com/link10",
+        "https://example.com/link11"
+    ],
+    ["https://example.com/link12", "https://example.com/link13", "https://example.com/link15"]
+];
+
+export { headings, images, links };
