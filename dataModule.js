595230b3cbc4006e0b5ac376dd78115bd06376bd
rjsearn <145315956+rjsearn@users.noreply.github.com>
2024-01-17 22:29:01 +0530

    Add files via upload

--- dataModule.js
+++ dataModule.js
@@ -1,29 +1,120 @@
-// dataModule.js
-
 const headings = [
-    ["Here we are step1", "Hae you dne it", "Here we are ending about", "KRSNA"],
-    ["This is set2 starting", "Hae you dne it", "KRSNA", "Can nnot do it"],
-    ["set3 Can nnot do it", "MAAm Ekam", "Here we are starting"]
-];
-
-const images = [
-    ["https://i.imgur.com/VgvPhWq.png", "https://i.imgur.com/6LPnBYO.png",
-        "https://i.imgur.com/M4tjdab.png", "https://i.imgur.com/wuuoabf.png"
+    ["Join LeadsLeap and Start EasyBuilder Setup"
     ],
-    ["https://i.imgur.com/dfNjudB.png", "https://i.imgur.com/6LPnBYO.png",
-        "https://i.imgur.com/M4tjdab.png", "https://i.imgur.com/lbengMy.png"
+    ["Import EasyBuilder Main Page", 
+"Import EasyBuilder Main Page",
+"Import EasyBuilder Main Page",
+"Import EasyBuilder Main Page",
+"Import EasyBuilder Main Page"
     ],
-    ["https://i.imgur.com/dfNjudB.png", "https://i.imgur.com/LbnpjwK.png",
-        "https://i.imgur.com/M4tjdab.png"
+    ["Import EasyBuilder Option Page",
+"Import EasyBuilder Option Page",
+"Import EasyBuilder Option Page",
+"Import EasyBuilder Option Page",
+"Import EasyBuilder Option Page"
+    ],
+    ["Create a new Email List",
+"Create a new Email List",
+"Create a new Email List",
+"Create a new Email List",
+"Create a new Email List",
+"Create a new Email List",
+"Create a new Email List",
+"Create a new Email List",
+"Create a new Email List"
+    ],
+    ["Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page",
+"Create EasyBuilder Splash Page"
     ]
 ];
 
+const images = [
+    ["https://i.imgur.com/QqrfSTQ.png"
+    ],
+    ["https://i.imgur.com/g50bBe5.png",
+"https://i.imgur.com/zgGN6QX.png",
+"https://i.imgur.com/zwt3gA6.png",
+"https://i.imgur.com/Dlaxbwp.png",
+"https://i.imgur.com/4oupKdj.png"
+    ],
+    ["https://i.imgur.com/vhQflkx.png",
+"https://i.imgur.com/zgGN6QX.png",
+"https://i.imgur.com/mGfgFJ4.png",
+"https://i.imgur.com/4qwXLHR.png",
+"https://i.imgur.com/R5S0TID.png"
+    ],
+    ["https://i.imgur.com/u8TQoHk.png",
+"https://i.imgur.com/9IYc8qp.png",
+"https://i.imgur.com/iiuevqk.png",
+"https://i.imgur.com/Dd3RXs4.png",
+"https://i.imgur.com/tzbDNDs.png",
+"https://i.imgur.com/A9JFjKY.png",
+"https://i.imgur.com/Kssy5Wo.png",
+"https://i.imgur.com/ntSRDsM.png",
+"https://i.imgur.com/2WDQuWA.png"
+], 
+    ["https://i.imgur.com/8gjC9Dg.png",
+"https://i.imgur.com/40luHIO.png",
+"https://i.imgur.com/qK5b7tL.png",
+"https://i.imgur.com/bhUU4eq.png",
+"https://i.imgur.com/G7hvRBx.png",
+"https://i.imgur.com/VkhcKEi.png",
+"https://i.imgur.com/oFUlgl9.png",
+"https://i.imgur.com/EJGZRIY.png",
+"https://i.imgur.com/hlpGdiA.png",
+"https://i.imgur.com/yyivHmh.png",
+"https://i.imgur.com/rQIq8FY.png",
+"https://i.imgur.com/hnc4uid.png"
+     ]
+    
+];
+
 const links = [
-    ["https://example.com/link1", "https://example.com/link2", "https://example.com/link3",
-        "https://example.com/link4"
+    ["https://leadsleap.com/?r=rjsearn"
     ],
-    ["https://example.com/link6", "https://example.com/link7", "https://example.com/link10",
-        "https://example.com/link11"
+    ["https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn"
     ],
-    ["https://example.com/link12", "https://example.com/link13", "https://example.com/link15"]
+    ["https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn"
+    ],
+    ["https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn"
+    ],
+    ["https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn", 
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn",
+"https://leadsleap.com/?r=rjsearn"
+    ]
 ];
