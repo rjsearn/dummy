1651413fa503ce12e84ddfc9d4cfbd66d9c28c6d
rjsearn <145315956+rjsearn@users.noreply.github.com>
2024-01-24 12:47:40 +0530

    Add files via upload

--- tr/script.js
+++ tr/script.js
@@ -1,24 +1,30 @@
 let currentIndex = 0;
 let currentSet = 0;
 
-    function showNextImage() {
-        if (currentIndex < getCurrentSetLength() - 1) {
-            currentIndex = (currentIndex + 1) % getCurrentSetLength();
+  function showNextImage() {
+    if (currentIndex < getCurrentSetLength() - 1) {
+        currentIndex = (currentIndex + 1) % getCurrentSetLength();
+    } else {
+        // If the current image is the last one, hide the "Next Page" button
+        document.getElementById("nextButton").style.display = "none";
+    }
+
+    document.getElementById("displayedImage").src = images[currentSet][currentIndex];
+    document.querySelector(".start-here-link").href = links[currentSet][currentIndex];
+    updateHeading();
+}
+
+function showPreviousImage() {
+    // Always show the "Next Page" button when going back to a previous image
+    document.getElementById("nextButton").style.display = "block";
+
+    if (currentIndex > 0) {
+        currentIndex = (currentIndex - 1) % getCurrentSetLength();
         document.getElementById("displayedImage").src = images[currentSet][currentIndex];
         document.querySelector(".start-here-link").href = links[currentSet][currentIndex];
         updateHeading();
-       } 
-        
     }
-    	
-	function showPreviousImage() {
-        if (currentIndex > 0) {
-            currentIndex = (currentIndex - 1) % getCurrentSetLength();
-            document.getElementById("displayedImage").src = images[currentSet][currentIndex];
-            document.querySelector(".start-here-link").href = links[currentSet][currentIndex];
-            updateHeading();
-        }
-    }
+}
 
     function updateHeading() {
         document.getElementById("imageHeading").textContent = headings[currentSet][currentIndex];
