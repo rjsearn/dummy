a7de722e0396a380dd59caa4a74ce25918224053
rjsearn <145315956+rjsearn@users.noreply.github.com>
2024-01-27 18:27:46 +0530

    Update mobile.js

--- tr/mobile.js
+++ tr/mobile.js
@@ -1,3 +1,4 @@
+/* Mobile Menu Toggle */
 function toggleMenu() {
             var mobileMenu = document.getElementById('mobileMenu');
             mobileMenu.style.display = (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') ? 'block' : 'none';
@@ -10,4 +11,22 @@ function toggleMenu() {
             if (mobileMenu.style.display === 'block' && e.target !== mobileMenu && e.target !== mobileMenuToggle && !mobileMenu.contains(e.target)) {
                 mobileMenu.style.display = 'none';
             }
+});
+
+/* Main Menu Highlight */
+document.addEventListener("DOMContentLoaded", function () {
+            // Get the current page URL
+            var currentPage = window.location.href;
+
+            // Select all menu links
+            var menuLinks = document.querySelectorAll('.main-menu a');
+
+            // Iterate through each link
+            menuLinks.forEach(function (link) {
+                // Check if the link's href matches the current page URL
+                if (link.href === currentPage) {
+                    // Add 'active' class to the link if it matches
+                    link.classList.add('active');
+                }
+            });
 });
