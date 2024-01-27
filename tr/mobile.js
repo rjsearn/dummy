6cd2cd11ad5cad200ec69f7609ab68f393ac7f68
rjsearn <145315956+rjsearn@users.noreply.github.com>
2024-01-27 19:05:26 +0530

    Update mobile.js

--- tr/mobile.js
+++ tr/mobile.js
@@ -30,3 +30,21 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             });
 });
+
+/* Mobile Menu Highlight */
+document.addEventListener("DOMContentLoaded", function () {
+            // Get the current page URL
+            var currentPage = window.location.href;
+
+            // Select all menu links
+            var menuLinks = document.querySelectorAll('.mobile-menu a');
+
+            // Iterate through each link
+            menuLinks.forEach(function (link) {
+                // Check if the link's href matches the current page URL
+                if (link.href === currentPage) {
+                    // Add 'active' class to the link if it matches
+                    link.classList.add('active');
+                }
+            });
+});
